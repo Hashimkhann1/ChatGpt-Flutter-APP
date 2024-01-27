0ec8ea4d2b7074ced52bf4a31d1b318fbea5cb63
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-27 18:44:25 +0500

    colors and one list is added

---
+++ lib/res/constant.dart
@@ -0,0 +1,37 @@
+
+
+
+
+import 'package:flutter/cupertino.dart';
+
+Color scafoldBackgroundColor = const Color(0xFF343541);
+Color cardColor = const Color(0xFF444654);
+
+
+final chatMessages = [
+  {
+    "msg":"Hello Who are you?",
+    "chatIndex" : 0,
+  },
+  {
+    "msg":"Hello, i'm ChatGpt, a large language model developed by OpenAI. I am here to assest you",
+    "chatIndex" : 1,
+  },
+  {
+    "msg":"What is flutter?",
+    "chatIndex" : 0,
+  },
+  {
+    "msg":"Flutter is an open-source mobile application development framework created by Google",
+    "chatIndex" : 1,
+  },
+  {
+    "msg":"What is flutter?",
+    "chatIndex" : 0,
+  },
+  {
+    "msg":"Flutter is an open-source mobile application development framework created by Google",
+    "chatIndex" : 1,
+  },
+
+];
